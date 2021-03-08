7cd074ce414e357151163a381fe38bdf4982880c
roshanbellary <40147115+roshanbellary@users.noreply.github.com>
2021-03-08 11:46:17 -0800

    Update

--- FEB_2021/Years.java
+++ FEB_2021/Years.java
@@ -1,47 +1,24 @@
 import java.io.*;
 import java.util.*;
 public class Years {
-    public static ArrayList<Edge>[] e;
     public static int N;
     public static int difference=0;
     public static String[] years = {"Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig","Rat"};
-    public static boolean[] visited;
-    public static class Edge{
-        int i,w;
-        public Edge(int _i, int _w){
-            i=_i;
-            w=_w;
-        }
-    }
     public static class Cow{
         int y;
         String name;
-        public Cow(int _y, String _name){
+        int diff;
+        public Cow(int _y, String _name, int diff){
             y=_y;
             name=_name;
-        }
-    }
-    public static void dfs(int start, int finish, boolean[] visited, int diff){
-        if (start==finish){
-            difference=diff;
-            return;
-        }
-        visited[start]=true;
-        for (int i=0;i<e[start].size();i++){
-            if (!visited[e[start].get(i).i]){
-                Years.dfs(e[start].get(i).i, finish, visited, diff+e[start].get(i).w);
-            }
+            this.diff=diff;
         }
     }
     public static void main(String[] args)throws IOException{
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(r.readLine());
-        e = new ArrayList[N+1];
-        for (int i=0;i<N+1;i++){
-            e[i]=new ArrayList<Edge>();
-        }
         ArrayList<Cow> cows = new ArrayList<Cow>();
-        cows.add(new Cow(0,"Bessie"));
+        cows.add(new Cow(0,"Bessie",0));
         for (int i=0;i<N;i++){
             int direction = 0;
             String[] store = r.readLine().split(" ");
@@ -52,50 +29,29 @@ public class Years {
             }else{
                 direction=1;
             }
-            int ind_f = -1;
-            int ind_s = -1;
-            for (int j=0;j<cows.size();j++){
-                if (cows.get(j).name.equals(name1)){
-                    ind_f=j;
-                }
-                if (cows.get(j).name.equals(name2)){
-                    ind_s=j;
+            int index = -1;
+            for (int z=0;z<cows.size();z++){
+                if (cows.get(z).name.equals(name2)){
+                    index=z;
                 }
             }
             int diff = -1;
             for (int j=0;j<12;j++){
                 if (years[j].equals(store[4])){
-                    diff=j;
+                    diff = j;
                 }
             }
-            if (ind_f<0){
-                cows.add(new Cow(diff, name1));
-                ind_f=cows.size()-1;
-            }
-            if (ind_s<0){
-                cows.add(new Cow(diff, name2));
-                ind_s=cows.size()-1;
-            }
             if (direction>0){
-                e[ind_f].add(new Edge(ind_s, -direction*(12-(cows.get(ind_s).y-diff))));
-                e[ind_s].add(new Edge(ind_f, direction*(12-(cows.get(ind_s).y-diff)))); 
+                cows.add(new Cow(diff, name1, cows.get(index).diff+12+diff-cows.get(index).y));
             }else{
-                e[ind_f].add(new Edge(ind_s, -direction*(12+cows.get(ind_s).y-diff)));
-                e[ind_s].add(new Edge(ind_f, direction*(12+cows.get(ind_s).y-diff)));
+                cows.add(new Cow(diff, name1, cows.get(index).diff-12-cows.get(index).y+diff));
             }
         }
-        int ind_b = -1;
-        int ind_e=-1;
-        for (int i=0;i<N;i++){
-            if (cows.get(i).name.equals("Bessie")){
-                ind_b=i;
-            }
+        for (int i=0;i<cows.size();i++){
             if (cows.get(i).name.equals("Elsie")){
-                ind_e=i;
+                difference=cows.get(i).diff;
             }
         }
-        visited = new boolean[cows.size()];
-        Years.dfs(ind_b, ind_e, visited,0);
         System.out.println(Math.abs(difference));
     }
 }
