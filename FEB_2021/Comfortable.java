3cd7d83973e52c0492a4b19b13e13354acd41c5d
roshanbellary <40147115+roshanbellary@users.noreply.github.com>
2021-03-20 16:19:49 -0700

    Full Solution to Problem 2 USACO

--- FEB_2021/Comfortable.java
+++ FEB_2021/Comfortable.java
@@ -1,37 +1,74 @@
 import java.io.*;
 import java.util.*;
 public class Comfortable{
+    public static boolean[][] pos = new boolean[1001][1001];
     public static int N;
-    public static Points[] g;
-    public static class Points{
-        int x,y;
-        public Points(int _x, int _y){
-            x=_x;
-            y=_y;
-        }
-        public int compareTo(Points n){
-            if (x==n.x){
-                return Integer.compare(y,n.y);
-            }else{
-                return Integer.compare(x, n.x);
+    public static int[] results;
+    public static boolean check(int x, int y){
+        if (x>1000||x<0||y<0||y>1000){
+            return false;
+        }else{
+            if (!pos[x][y]){
+                return false;
             }
         }
-    }
-    public static void count(int i){
-        Points[] j = new Points[i+1];
-        for (int z=0;z<=i;z++){
-            j[z]=g[z];
+        int num = 0;
+        if ((x-1)>=0){
+            if (pos[x-1][y]){
+                num++;
+            }
         }
-        Arrays.sort(j);
+        if (x+1<=1000){
+            if (pos[x+1][y]){
+                num++;
+            }
+        }
+        if ((y-1)>=0){
+            if (pos[x][y-1]){
+                num++;
+            }
+        }
+        if (y+1<=1000){
+            if (pos[x][y+1]){
+                num++;
+            }
+        }
+        if (num==3){
+            return true;
+        }
+        return false;
     }
     public static void main(String[] args)throws IOException{
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(r.readLine());
-        g = new Points[N];
+        results = new int[N];
+        int count=0;
         for (int i=0;i<N;i++){
             String[] store = r.readLine().split(" ");
-            g[i] = new Points(Integer.parseInt(store[0]),Integer.parseInt(store[1]));
+            int posx = Integer.parseInt(store[0]);
+            int posy = Integer.parseInt(store[1]);
+            int old=0;
+            if (Comfortable.check(posx-1,posy)) old++;
+            if (Comfortable.check(posx+1,posy)) old++;
+            if (Comfortable.check(posx,posy+1)) old++;
+            if (Comfortable.check(posx,posy-1)) old++;
+            count-=old;
+            pos[posx][posy]=true;
+            int newc=0;
+            if (Comfortable.check(posx-1,posy)) newc++;
+            if (Comfortable.check(posx+1,posy)) newc++;
+            if (Comfortable.check(posx,posy+1)) newc++;
+            if (Comfortable.check(posx,posy-1)) newc++;
+            if (Comfortable.check(posx,posy)) newc++;
+            count+=newc;
+            if (count<0){
+                count=0;
+            }
+            results[i]=count;
         }
         r.close();
+        for (int i=0;i<N;i++){
+            System.out.println(results[i]);
+        }
     }
 }
